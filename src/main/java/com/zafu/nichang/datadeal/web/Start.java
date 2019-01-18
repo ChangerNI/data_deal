a287f5e195f1fb17f80d70c6e1b3364124a120e7
朱文赵 <zwz@servyou.com.cn>
2019-01-18 10:26:27 +0800

    重构，优化Start类，并提出修改建议和下一步的编写计划

--- src/main/java/com/zafu/nichang/datadeal/web/Start.java
+++ src/main/java/com/zafu/nichang/datadeal/web/Start.java
@@ -12,13 +12,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.function.Function;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -33,77 +31,102 @@ public class Start {
     /**
      * 日志
      */
-      private static final Logger logger = LoggerFactory.getLogger(Start.class);
+    private static final Logger logger = LoggerFactory.getLogger(Start.class);
     /**
      * ROOT_DIRECTORY
      * 根目录
      */
     private static final String ROOT_DIRECTORY = System.getProperty("user.dir");
 
-    public static final ExecutorService executorService = Executors.newFixedThreadPool(5,
-            new BasicThreadFactory.Builder().namingPattern("excel-writer-%d").daemon(true).build());
+    public static ExecutorService htmlParserExecutorService = Executors.newFixedThreadPool(5,
+            new BasicThreadFactory.Builder().namingPattern("html-parser-%d").daemon(true).build());
 
 
     /**
      * 数据库连接
      */
-    private static Connection conn=null;
+    private static Connection conn = null;
 
     /**
      * 主方法  从程序入口
+     *
      * @param args
      * @throws Exception
      */
-      public static void main(String [] args){
-          try {
-              //读取配置文件
-              PropertiesModel pm = PropertiesUtil.getProperties(ROOT_DIRECTORY + "/config/propertires_web.properties");
-              logger.info("配置文件读取成功！");
-              //连接源数据库
-              conn = DbUtil.getConnection(pm.getDriver(), pm.getUrl(), pm.getUsername(), pm.getPassword());
-              logger.info("数据库连接成功！");
-              int maxPageVegetable = getMaxPage(OkHttpUtil.getHtmlByOkHttp(pm.getWebVegetableUrl().replace("???", String.valueOf(1)), ""));
-              int maxPageFruit = getMaxPage(OkHttpUtil.getHtmlByOkHttp(pm.getWebFruitUrl().replace("???", String.valueOf(1)), ""));
-              int maxPageMeat = getMaxPage(OkHttpUtil.getHtmlByOkHttp(pm.getWebMeatUrl().replace("???", String.valueOf(1)), ""));
-              int maxPageAquatic = getMaxPage(OkHttpUtil.getHtmlByOkHttp(pm.getWebAquaticUrl().replace("???", String.valueOf(1)), ""));
-              int maxPageOil = getMaxPage(OkHttpUtil.getHtmlByOkHttp(pm.getWebOilUrl().replace("???", String.valueOf(1)), ""));
+    public static void main(String[] args) {
+        try {
+            //读取配置文件
+            PropertiesModel pm = PropertiesUtil.getProperties(ROOT_DIRECTORY + "/config/propertires_web.properties");
+            logger.info("配置文件读取成功！");
+            //连接源数据库
+            conn = DbUtil.getConnection(pm.getDriver(), pm.getUrl(), pm.getUsername(), pm.getPassword());
+            logger.info("数据库连接成功！");
+            int maxPageVegetable = getMaxPage(OkHttpUtil.getHtmlByOkHttp(pm.getWebVegetableUrl().replace("???", String.valueOf(1)), ""));
+            int maxPageFruit = getMaxPage(OkHttpUtil.getHtmlByOkHttp(pm.getWebFruitUrl().replace("???", String.valueOf(1)), ""));
+            int maxPageMeat = getMaxPage(OkHttpUtil.getHtmlByOkHttp(pm.getWebMeatUrl().replace("???", String.valueOf(1)), ""));
+            int maxPageAquatic = getMaxPage(OkHttpUtil.getHtmlByOkHttp(pm.getWebAquaticUrl().replace("???", String.valueOf(1)), ""));
+            int maxPageOil = getMaxPage(OkHttpUtil.getHtmlByOkHttp(pm.getWebOilUrl().replace("???", String.valueOf(1)), ""));
 
-              logger.info("解析最大网页数成功！");
-              List<Product> result = new LinkedList<>();
+            logger.info("解析最大网页数成功！");
+            List<Product> result = new LinkedList<>();
 
-              System.out.println(maxPageVegetable+"\n"+maxPageFruit+"\n"+maxPageMeat+"\n"+maxPageAquatic+"\n"+maxPageOil+"\n");
+            logger.info("maxPageVegetable: {}, maxPageFruit: {}, maxPageMeat;{}, maxPageOil: {}",
+                    maxPageVegetable, maxPageFruit, maxPageMeat, maxPageAquatic, maxPageOil);
 
-              for(int i=1; i<10; i++) {
-                  String url = pm.getWebVegetableUrl().replace("???", String.valueOf(i));
-                  String htmlPage = OkHttpUtil.getHtmlByOkHttp(url, "");
-                  logger.info("解析网页成功！");
-                  List<String> htmlBlocks = RegUtil.getRegInfoBlocks(Constant.OTA_WEB_HTML_BLOCK_REG_PATTERN, htmlPage);
+            // 获得不同页面的产品列表
+            // todo 开启多线程 此处你来写 用上面的htmlParserExecutorService 线程池
+            // todo 测试的时候可以将count的这个字段随便写个值进去
+            List<Product> product = getProduct(maxPageVegetable, PropertiesModel::getWebVegetableUrl, pm);
+            // 提供一个思路
+            // 线程采用Callable 然后for循环 再用下面的方法
+            //htmlParserExecutorService.submit(callable)
+            // 拿到返回值
+            // 这里可以 每个线程都去数据库中插入了 就不用再交个主线程一起插入
+            // 即 上面的result的这个列表就不要addAll了
 
-                  List<Product> productList = htmlBlocks.stream().map(Start::getProduct).collect(toList());
-                  result.addAll(productList);
-                  logger.info("当前页：{}", productList);
 
-              }
+            logger.info("所有产品列表：{}", result);
 
+        } catch (Exception e) {
+            logger.info("error：", e);
+        } finally {
+            try {
+                //关闭数据库连接
+                conn.close();
+            } catch (Exception e) {
+                logger.info(e.toString());
+            }
+        }
+    }
 
-              logger.info("所有产品列表：{}", result);
-
-          }catch (Exception e){
-                logger.info("error：", e);
-          }finally {
-              try{
-                  //关闭数据库连接
-                  conn.close();
-              }catch (Exception e){
-                  logger.info(e.toString());
-              }
-          }
-      }
-
+    /**
+     * 获得页面产品
+     * 采用java8中{@link Function} 来重构模板方法 不同的地方已经抽象出来
+     *
+     * @param pageCount 不同的页面产品列表
+     * @param getUrl 获得不同的页面的url的方法
+     * @param propertiesModel propertiesModel
+     * @return
+     * @throws Exception
+     */
+    private static List<Product> getProduct(int pageCount, Function<PropertiesModel, String> getUrl,
+                                            PropertiesModel propertiesModel) throws Exception {
+        List<Product> productList = new LinkedList<>();
+        for (int i = 1; i < pageCount; i++) {
+            String url = getUrl.apply(propertiesModel).replace("???", String.valueOf(i));
+            String htmlPage = OkHttpUtil.getHtmlByOkHttp(url, "");
+            logger.info("解析网页成功！");
+            List<String> currentPageHtmlBlocks = RegUtil.getRegInfoBlocks(Constant.OTA_WEB_HTML_BLOCK_REG_PATTERN, htmlPage);
+            List<Product> currentPageProductList = currentPageHtmlBlocks.stream().map(Start::getProduct).collect(toList());
+            productList.addAll(currentPageProductList);
+        }
+        return productList;
+    }
 
 
     /**
      * 拼接三个产品特征LinkedList
+     *
      * @param pattern
      * @param htmlBlocks
      * @return
@@ -124,10 +147,11 @@ public class Start {
 
     /**
      * 获得网页最大页数
+     *
      * @param html
      * @return
      */
-    private static int getMaxPage(String html){
+    private static int getMaxPage(String html) {
         return RegUtil.getMaxPage(Constant.OTA_WEB_HTML_LAST_PAGE_REG_PATTERN, html);
     }
 
